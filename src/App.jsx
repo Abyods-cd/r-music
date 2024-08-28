ca47b023effc1203be510170ac92aa888ca82499
Vod <avod.zy@gmail.com>
2024-08-28 23:01:18 +0800

    setup: deleted unrelated files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types'
+import React, { memo } from 'react'
+
+const App = memo((props) => {
+  return (
+    <div>
+      R Music
+    </div>
+  )
+})
+
+App.propTypes = {}
+
+export default App
